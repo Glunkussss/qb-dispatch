2cb8d9723f3193371b344e92cc8a49b6b5ea2e83
Mauricio <102635797+Glunkussss@users.noreply.github.com>
2022-07-08 23:13:10 -0500

    Beta 1.0
    Dispatch

---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+
+/*
+
+[DO NOT SELL THIS SHIT ITS FREE, ITS A EDIT OF THE GUILLE DISPATCH.]
+
+*/
+fx_version 'adamant'
+author 'Glunkus#2785'
+description 'Guille dispatch a QBUS'
+game 'gta5'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/*.html',
+    'ui/*.js',
+    'ui/vehicles/*.png',
+    'ui/vehicles/*.webp',
+    'ui/*.css',
+    'ui/*.svg',
+    'ui/*.png',
+    'ui/*.ttf'
+    
+}
